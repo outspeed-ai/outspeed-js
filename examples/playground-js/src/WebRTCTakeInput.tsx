9188765b7d0177baded491840d17835ed732778c
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-09-23 20:10:39 +0900

    add basic form validation

--- examples/playground-js/src/WebRTCTakeInput.tsx
+++ examples/playground-js/src/WebRTCTakeInput.tsx
@@ -16,8 +16,38 @@ export function WebRTCTakeInput(props: TWebRTCTakeInputProps) {
   const [audioDeviceId, setAudioDeviceId] = React.useState("");
   const [videoDeviceId, setVideoDeviceId] = React.useState("");
   const [functionURL, setFunctionURL] = React.useState("http://localhost:8080");
+  const [isMediaMissing, setIsMediaMissing] = React.useState(false);
+  const [isFunctionURLMissing, setIsFunctionURLMissing] = React.useState(false);
+
+  function handleOnMediaInputChange(kind: "audio" | "video", value: string) {
+    setIsMediaMissing(false);
+
+    switch (kind) {
+      case "audio":
+        setAudioDeviceId(value);
+        break;
+      case "video":
+        setVideoDeviceId(value);
+        break;
+    }
+  }
 
   function handleFormSubmit() {
+    let isFormValid = true;
+    if (!audioDeviceId || !videoDeviceId) {
+      setIsMediaMissing(true);
+      isFormValid = false;
+    }
+
+    if (!functionURL) {
+      setIsFunctionURLMissing(true);
+      isFormValid = false;
+    }
+
+    if (!isFormValid) {
+      return;
+    }
+
     try {
       const config = createConfig({
         functionURL,
@@ -35,19 +65,32 @@ export function WebRTCTakeInput(props: TWebRTCTakeInputProps) {
     <div className="space-y-6 max-w-lg relative z-10">
       <div className="font-bold text-3xl mb-8">WebRTC</div>
       <RealtimeFunctionURLInput
-        onChange={(e) => setFunctionURL(e.currentTarget.value)}
+        isError={isFunctionURLMissing}
+        onChange={(e) => {
+          setIsFunctionURLMissing(false);
+          setFunctionURL(e.currentTarget.value);
+        }}
         value={functionURL}
         description="Once you've deployed your WebRTC backend application, you'll receive a URL. If you are running your backend locally, use http://localhost:8080."
+        errorMsg={isFunctionURLMissing ? "Function url is required." : ""}
       />
       <RealtimeAudioInput
+        isError={isMediaMissing}
         value={audioDeviceId}
-        onChange={setAudioDeviceId}
+        onChange={(value) => handleOnMediaInputChange("audio", value)}
         description="Select the microphone you want to use. If you don't see your microphone, make sure it is plugged in."
+        errorMsg={
+          isMediaMissing ? "Either audio or video input is required." : ""
+        }
       />
       <RealtimeVideoInput
+        isError={isMediaMissing}
         value={videoDeviceId}
-        onChange={setVideoDeviceId}
+        onChange={(value) => handleOnMediaInputChange("video", value)}
         description="Select the camera you want to use. If you don't see your camera, make sure it is plugged in."
+        errorMsg={
+          isMediaMissing ? "Either audio or video input is required." : ""
+        }
       />
       <RealtimeFormButton onClick={handleFormSubmit}>Run</RealtimeFormButton>
     </div>
--- examples/playground-js/src/WebSocketTakeInput.tsx
+++ examples/playground-js/src/WebSocketTakeInput.tsx
@@ -16,8 +16,24 @@ export function WebSocketTakeInput(props: TWebSocketTakeInputProps) {
   const [functionURL, setFunctionURL] = React.useState(
     "http://localhost:8080/"
   );
+  const [isAudioMissing, setIsAudioMissing] = React.useState(false);
+  const [isFunctionURLMissing, setIsFunctionURLMissing] = React.useState(false);
 
   function handleFormSubmit() {
+    let isFormValid = true;
+    if (!audioDeviceId) {
+      setIsAudioMissing(true);
+      isFormValid = false;
+    }
+
+    if (!functionURL) {
+      setIsFunctionURLMissing(true);
+      isFormValid = false;
+    }
+
+    if (!isFormValid) {
+      return;
+    }
     try {
       const config = {
         functionURL,
@@ -38,14 +54,24 @@ export function WebSocketTakeInput(props: TWebSocketTakeInputProps) {
     <div className="space-y-6 max-w-lg relative z-10">
       <div className="font-bold text-3xl mb-8">Web Socket</div>
       <RealtimeFunctionURLInput
-        onChange={(e) => setFunctionURL(e.currentTarget.value)}
+        isError={isFunctionURLMissing}
+        onChange={(e) => {
+          setIsFunctionURLMissing(false);
+          setFunctionURL(e.currentTarget.value);
+        }}
         value={functionURL}
         description="Once you've deployed your WebSocket backend application, you'll receive a URL. If you are running your backend locally, use http://localhost:8080."
+        errorMsg={isFunctionURLMissing ? "Function url is required." : ""}
       />
       <RealtimeAudioInput
+        isError={isAudioMissing}
         value={audioDeviceId}
-        onChange={setAudioDeviceId}
+        onChange={(value) => {
+          setIsAudioMissing(false);
+          setAudioDeviceId(value);
+        }}
         description="Select the microphone you want to use. If you don't see your microphone, make sure it is plugged in."
+        errorMsg={isAudioMissing ? "Please select audio device." : ""}
       />
       <RealtimeFormButton onClick={handleFormSubmit}>Run</RealtimeFormButton>
     </div>
