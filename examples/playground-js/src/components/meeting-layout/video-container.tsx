3829f8043eb79c2c5d6b851c63d88857a4c21d8f
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-09-25 04:15:24 +0900

    fix max-height

--- examples/playground-js/src/components/meeting-layout/video-container.tsx
+++ examples/playground-js/src/components/meeting-layout/video-container.tsx
@@ -15,7 +15,7 @@ export function VideContainer(props: TVideContainerProps) {
   const { track, label, hasControls } = props;
 
   return (
-    <div className="flex-1 relative group rounded-lg border">
+    <div className="flex-1 relative group rounded-lg border max-h-72 sm:max-h-[500px]">
       {hasControls && (
         <div className="absolute top-2 right-2 opacity-0 space-x-3 group-hover:opacity-100">
           {track && <MediaAction track={track} Off={VideoOff} On={Video} />}
