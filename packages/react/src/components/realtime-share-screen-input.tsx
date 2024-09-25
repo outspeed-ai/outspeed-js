b67e76403b0f34fe230fb6a8d2cf8a7eb903b288
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-09-25 22:42:27 +0900

    fix realtime-share-screen-input component

--- packages/react/src/components/realtime-share-screen-input.tsx
+++ packages/react/src/components/realtime-share-screen-input.tsx
@@ -28,6 +28,15 @@ export type TRealtimeShareScreenInputProps = {
    * Callback when the value changes.
    */
   onChange: (value: string) => void;
+  /**
+   * If then border and label color will be red.
+   */
+  isError?: boolean;
+
+  /**
+   * If defined then error msg will be shown below description.
+   */
+  errorMsg?: string;
 };
 
 export function RealtimeShareScreenInput(
@@ -39,13 +48,17 @@ export function RealtimeShareScreenInput(
     placeholder = "Do you want to share your screen",
     label = "Screen",
     description,
+    isError,
+    errorMsg,
   } = props;
 
   return (
     <FormItem>
-      <FormLabel htmlFor="share-screen">{label}</FormLabel>
+      <FormLabel htmlFor="share-screen" isError={isError}>
+        {label}
+      </FormLabel>
       <Select onValueChange={onChange} value={value}>
-        <SelectTrigger id="share-screen">
+        <SelectTrigger isError={isError} id="share-screen">
           <SelectValue placeholder={placeholder} />
         </SelectTrigger>
         <SelectContent>
@@ -60,6 +73,7 @@ export function RealtimeShareScreenInput(
         </SelectContent>
       </Select>
       {description && <FormDescription>{description}</FormDescription>}
+      {errorMsg && <FormDescription isError>{errorMsg}</FormDescription>}
     </FormItem>
   );
 }
