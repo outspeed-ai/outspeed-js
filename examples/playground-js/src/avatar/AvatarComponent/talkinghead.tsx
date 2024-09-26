9ccc64bd3027482ef17662a9c500871971384136
Sahil Manocha <sahilmanocha1994@gmail.com>
2024-09-25 20:52:51 -0700

    removed ts-check in talkinghead.tsx

--- examples/playground-js/src/avatar/AvatarComponent/talkinghead.tsx
+++ examples/playground-js/src/avatar/AvatarComponent/talkinghead.tsx
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 import * as THREE from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
