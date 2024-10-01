eb66d1b402f4e716a3e0093fc1ff2d1f5bfccdac
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-10-01 16:34:23 +0900

    add react native sdk place holder

---
+++ packages/react-native-sdk/src/Div.tsx
@@ -0,0 +1,5 @@
+import { Text, type TextProps } from "react-native";
+
+export function Div(props: TextProps) {
+  return <Text {...props} />;
+}
---
+++ packages/react-native-sdk/tsup.config.ts
@@ -0,0 +1,20 @@
+import { defineConfig } from "tsup";
+import { copyFileSync } from "fs";
+import { join, resolve } from "path";
+
+export default defineConfig({
+  entry: ["src/**/*.(ts|tsx)"],
+  dts: true,
+  format: ["cjs", "esm"],
+  clean: true,
+  outDir: "dist",
+  tsconfig: "./tsconfig.json",
+  sourcemap: true,
+  minify: false,
+  splitting: false,
+  onSuccess: async () => {
+    const destPath = join(__dirname, "dist");
+    const licensePath = join(__dirname, "..", "..", "LICENSE");
+    copyFileSync(resolve(licensePath), join(destPath, "LICENSE"));
+  },
+});
