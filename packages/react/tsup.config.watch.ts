70265cf61e408e529e7ad192d8559d75353d4012
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-10-14 14:24:46 +0900

    refactor watch

--- packages/react/tsup.config.watch.ts
+++
@@ -1,8 +0,0 @@
-import buildConfig from "./tsup.config";
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  ...buildConfig,
-  dts: false,
-  tsconfig: "./tsconfig.watch.json",
-});
