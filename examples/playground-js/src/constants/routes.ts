dbea312df2d6ba40fe413a59743329f936c1fbb7
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-10-22 13:24:27 +0900

    fix build

--- examples/playground-js/src/constants/routes.ts
+++ examples/playground-js/src/constants/routes.ts
@@ -5,7 +5,6 @@ export const THANK_YOU_ROUTE = BASE_ROUTE + "thank_you";
 
 // Form
 export const VOICE_BOT_TAKE_INPUT_ROUTE = BASE_ROUTE + "voice_bot_input";
-export const WEBSOCKET_TAKE_INPUT_ROUTE = BASE_ROUTE + "websocket_input";
 export const HUMAN_AVATAR_TAKE_INPUT_ROUTE = BASE_ROUTE + "human_avatar_input";
 export const SPORTS_COMMENTATOR_TAKE_INPUT_ROUTE =
   BASE_ROUTE + "sports_commentator_input";
@@ -14,7 +13,6 @@ export const WEBSOCKET_TAKE_INPUT_ROUTE = BASE_ROUTE + "websocket_input";
 // App
 export const APP_ROUTE = BASE_ROUTE + "app/";
 export const VOICE_BOT_APP_ROUTE = APP_ROUTE + "voice_bot";
-export const WEBSOCKET_APP_ROUTE = APP_ROUTE + "web_socket";
 export const HUMAN_AVATAR_APP_ROUTE = APP_ROUTE + "human_avatar";
 export const SPORTS_COMMENTATOR_APP_ROUTE = APP_ROUTE + "sports_commentator";
 export const WEBSOCKET_APP_ROUTE = APP_ROUTE + "websocket";
