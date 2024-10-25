4a91f90af4292c180b36b6de8f192958e1215780
janak2 <janakagrawal1309@gmail.com>
2024-10-24 22:31:16 -0700

    export createConfig from react

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./hooks";
 export * from "./components";
+export { createConfig } from "@outspeed/core";
 export { ERealtimeConnectionStatus } from "@outspeed/core/connection-status";
