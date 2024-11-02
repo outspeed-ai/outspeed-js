0dff2435c4486154baaf85c5b71665c995c6bd2a
Ankit Kumar <ankits.kvmdam@hotmail.com>
2024-11-02 09:17:48 +0530

    add reset support for websocket

--- packages/react/src/hooks/useWebSocket.ts
+++ packages/react/src/hooks/useWebSocket.ts
@@ -4,12 +4,11 @@ import { RealtimeWebSocketConnection } from "@outspeed/core/RealtimeWebSocketCon
 import { TRealtimeWebSocketConfig, TResponse } from "@outspeed/core/@types";
 import { ERealtimeConnectionStatus } from "../connection-status";
 
-export type TUseWebSocketOptions = {
+export type TUseWebSocketConnectOptions = {
   config: TRealtimeWebSocketConfig;
 };
 
-export function useWebSocket(options: TUseWebSocketOptions) {
-  const { config } = options;
+export function useWebSocket() {
   const [connection, setConnection] =
     React.useState<RealtimeWebSocketConnection | null>(null);
   const [connectionStatus, setConnectionStatus] =
@@ -17,21 +16,26 @@ export function useWebSocket(options: TUseWebSocketOptions) {
   const [response, setResponse] = React.useState<TResponse>({});
   const [remoteTrack, setRemoteTrack] = React.useState<Track | null>(null);
 
-  const connect = React.useCallback(async () => {
-    setConnectionStatus(ERealtimeConnectionStatus.Connecting);
-    const ws = new RealtimeWebSocketConnection(config);
-    const response = await ws.connect();
-    if (!response.ok) {
-      // This will release media, if it is setup.
-      await ws.disconnect();
-      setConnectionStatus(ERealtimeConnectionStatus.Failed);
+  const connect = React.useCallback(
+    async (options: TUseWebSocketConnectOptions) => {
+      const { config } = options;
+
+      setConnectionStatus(ERealtimeConnectionStatus.Connecting);
+      const ws = new RealtimeWebSocketConnection(config);
+      const response = await ws.connect();
+      if (!response.ok) {
+        // This will release media, if it is setup.
+        await ws.disconnect();
+        setConnectionStatus(ERealtimeConnectionStatus.Failed);
+        setResponse(response);
+        return console.error("Failed to connect", response);
+      }
+      setConnection(ws);
+      setConnectionStatus(ERealtimeConnectionStatus.Connected);
       setResponse(response);
-      return console.error("Failed to connect", response);
-    }
-    setConnection(ws);
-    setConnectionStatus(ERealtimeConnectionStatus.Connected);
-    setResponse(response);
-  }, [config]);
+    },
+    []
+  );
 
   const disconnect = React.useCallback(async () => {
     if (!connection) {
@@ -41,7 +45,20 @@ export function useWebSocket(options: TUseWebSocketOptions) {
     setConnectionStatus(ERealtimeConnectionStatus.Disconnecting);
     await connection.disconnect();
     setConnectionStatus(ERealtimeConnectionStatus.Disconnected);
+    setRemoteTrack(null);
   }, [connection]);
+
+  const reset = React.useCallback(async () => {
+    if (
+      connection &&
+      connectionStatus === ERealtimeConnectionStatus.Connected
+    ) {
+      await disconnect();
+    }
+
+    setConnectionStatus(ERealtimeConnectionStatus.New);
+    setConnection(null);
+  }, [connection, connectionStatus, disconnect]);
 
   const localAudioTrack = React.useMemo(() => {
     if (!connection || connectionStatus !== ERealtimeConnectionStatus.Connected)
@@ -65,6 +82,7 @@ export function useWebSocket(options: TUseWebSocketOptions) {
   return {
     connect,
     disconnect,
+    reset,
     connectionStatus,
     dataChannel: connection?.dataChannel,
     localAudioTrack,
